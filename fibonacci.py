85fda41a3c88ae918d4daf8b4527bb3181e4f766
Zfred <142956501+Foumvn@users.noreply.github.com>
2023-08-23 04:52:35 +0200

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+
+from math import*
+
+x=int(input("Entrez le nombre d'iteration que vous desire"))
+a,b= 1,2
+print(a)
+print(b)
+
+racine= sqr(4)
+print(racine)
+ch="1618"
+a=float(ch)
+print(a+1)
+print(ch[0])
+
+
